6b31aa7694f8e71b3db1a1333610642bafd09ade
Jeremiah Buckley <jeremiah.buckley@gmail.com>
2017-01-14 17:03:06 -0500

    some work on improving the suggestions / format.

--- src/app/components/suggestions-list.component.js
+++ src/app/components/suggestions-list.component.js
@@ -2,6 +2,27 @@ class SuggestionsListController {
   constructor($state, $log) {
     this.$state = $state;
     this.$log = $log;
+    this.suggestions = [
+      {
+        _id: 3,
+        name: 'Tessa Smithers',
+        message: 'Turn the heat down, god dammit!',
+        read: false,
+        todo: false
+      }, {
+        _id: 2,
+        name: 'Reginald Quinn',
+        message: 'We\'d love to have more marmalade at breakfast. Those little packets go so fast!',
+        read: true,
+        todo: true
+      }, {
+        _id: 1,
+        name: 'Phyllis Oswagio',
+        message: 'Can you make it a little warmer? I hate having to wear all these sweaters!',
+        read: true,
+        todo: false
+      }
+    ];
   }
 }
 
